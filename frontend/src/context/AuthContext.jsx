b668b0c5072aa83665627071ef1408dd2c118e33
0xdeadw4r <unionunknown167@gmail.com>
2025-09-17 17:43:01 +0530

    Update AuthContext.jsx

--- frontend/src/context/AuthContext.jsx
+++ frontend/src/context/AuthContext.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import { createContext } from 'react'
 export const authDataContext= createContext()
 function AuthContext({children}) {
-    let serverUrl = "https://e-frozelia.onrender.com"
+    let serverUrl = "https://backend-frozelia.onrender.com"
 
     let value = {
        serverUrl
