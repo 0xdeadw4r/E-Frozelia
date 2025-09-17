519e1ecb2c17dc091e546e9167f772b50e5913ee
0xdeadw4r <unionunknown167@gmail.com>
2025-09-17 16:15:48 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -17,7 +17,7 @@ let app = express()
 app.use(express.json())
 app.use(cookieParser())
 app.use(cors({
- origin:["https://e-frozeliatwo.onrender.com" , "http://localhost:5174"],
+ origin:["https://e-frozeliatwo.onrender.com" , "https://e-frozelia-admin.onrender.com"],
  credentials:true
 }))
 
